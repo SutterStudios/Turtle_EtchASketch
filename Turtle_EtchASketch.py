dbbdcf4243de6abef9d8e4b52013c30466f1bfb0
Alex Sutter <46454849+suttera12@users.noreply.github.com>
2021-08-20 15:12:25 -0400

    Add files via upload

---
+++ Turtle_EtchASketch.py
@@ -0,0 +1,25 @@
+#Author: Alexander Sutter
+import turtle
+
+screen = turtle.Screen()
+
+move = 50 #steps
+turn = 90 #degrees
+
+def w():
+  turtle.fd(move)
+def a():
+  turtle.lt(turn)
+def s():
+  turtle.bk(move)
+def d():
+  turtle.rt(turn)
+
+
+screen.onkey(w, "Up")
+screen.onkey(s, "Down")
+screen.onkey(a, "Left")
+screen.onkey(d, "Right")
+
+screen.listen()
+turtle.done()
